406921e7b86c3372a0692f9513f953c423cdda31
Murod Qodirov <murodqodirov22@gmail.com>
2024-02-02 11:56:50 +0500

    migration, models, factory and seeders done

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostSeeder extends Seeder
 {
@@ -12,6 +14,9 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('posts')->truncate();
+        Post::factory(3)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,12 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class CommentSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,8 +15,8 @@ class CommentSeeder extends Seeder
     public function run(): void
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0');
-        DB::table('comments')->truncate();
-        Comment::factory(3)->create(); 
+        DB::table('users')->truncate();
+        User::factory(10)->create();
         DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 }
